ccfde438d4025888e182f1c69a72b1289ad4dcd0
mat-kossowski <mat.kossowski.pl@gmail.com>
2023-06-21 13:46:54 +0200

    creating an input testing class and adding tests to it

--- src/main/java/org/example/InputTesting.java
+++ src/main/java/org/example/InputTesting.java
@@ -1,2 +1,19 @@
-package org.example;public class InputTesting {
+package org.example;
+
+public class InputTesting {
+
+    public boolean validateTheInput(String input) {
+
+        if (input == null || input.isEmpty()) {
+            return false;
+        }
+
+        for (char c : input.toCharArray()) {
+            if (!Character.isDigit(c) && c != ' ') {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
--- src/test/java/org/example/InputTestingTest.java
+++ src/test/java/org/example/InputTestingTest.java
@@ -1,2 +1,100 @@
-package org.example;public class InputTestingTest {
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+
+public class InputTestingTest {
+    private InputTesting inputTesting;
+
+    @BeforeEach
+    public void setUp() {
+        inputTesting = new InputTesting();
+    }
+
+    @Test
+    public void testValidateTheInput_WithValidInput_ReturnsTrue() {
+        // Arrange
+        String input = "12345";
+
+        // Act
+        boolean result = inputTesting.validateTheInput(input);
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    public void testValidateTheInput_WithInvalidInput_ReturnsFalse() {
+        // Arrange
+        String input = "12a45";
+
+        // Act
+        boolean result = inputTesting.validateTheInput(input);
+
+        // Assert
+        assertFalse(result);
+    }
+
+    @Test
+    public void testValidateTheInput_WithEmptyInput_ReturnsFalse() {
+        // Arrange
+        String input = "";
+
+        // Act
+        boolean result = inputTesting.validateTheInput(input);
+
+        // Assert
+        assertFalse(result);
+    }
+
+    @Test
+    public void testValidateTheInput_WithNullInput_ReturnsFalse() {
+        // Arrange
+        String input = null;
+
+        // Act
+        boolean result = inputTesting.validateTheInput(input);
+
+        // Assert
+        assertFalse(result);
+    }
+
+    @Test
+    public void testValidateTheInput_WithOnlySpaces_ReturnsTrue() {
+        // Arrange
+        String input = "     ";
+
+        // Act
+        boolean result = inputTesting.validateTheInput(input);
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    public void testValidateTheInput_WithMixedDigitsAndSpaces_ReturnsTrue() {
+        // Arrange
+        String input = "12 345  67";
+
+        // Act
+        boolean result = inputTesting.validateTheInput(input);
+
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    public void testValidateTheInput_WithSpecialCharacters_ReturnsFalse() {
+        // Arrange
+        String input = "12@45";
+
+        // Act
+        boolean result = inputTesting.validateTheInput(input);
+
+        // Assert
+        assertFalse(result);
+    }
 }
